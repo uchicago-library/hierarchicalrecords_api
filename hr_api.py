b7cb708fd937083a6e0b22432e45ae6216cee359
Brian Balsamo <balsamo@uchicago.edu>
2016-06-28 10:55:53 -0500

    APIResponse object.

--- hr_api.py
+++ hr_api.py
@@ -27,6 +27,56 @@ def retrieve_conf(conf_str):
 def build_validator(conf):
     return RecordValidator(conf)
 
+class APIResponse(Object):
+
+    _status = None
+    _data = None
+    _errors = None
+
+    def __init__(self, status, data=None, errors=None):
+        self.status = status
+        self.data = None
+        self.errors = None
+
+    def get_status(self):
+        return self._status
+
+    def set_status(self, status):
+        if status not in ['success', 'fail']:
+            raise ValueError("status MUST be 'success' or 'fail'")
+        self._status = status
+
+    def get_data(self):
+        return self._data
+
+    def set_data(self, data):
+        if not isinstance(data, dict) and data is not None:
+            raise ValueError("Data must be a dict")
+        self._data = data
+
+    def get_errors(self):
+        return self._errors
+
+    def set_errors(self, errors):
+        if errors == None:
+            self._errors = None
+            return
+        try:
+            for x in errors:
+                self.add_error(x)
+        except TypeError:
+            raise ValueError("errors must be an iterable")
+
+    def add_error(self, error):
+        if not isinstance(error, str):
+            raise ValueError("error must be a string")
+        self._errors.append(error)
+
+    errors = property(get_errors, set_errors)
+    data = property(get_data, set_data)
+    status = property(get_status, set_status)
+
+
 class Root(Resource):
     def get(self):
         docs = """
