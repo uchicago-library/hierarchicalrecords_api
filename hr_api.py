cdfd212e0f5140877ddb35690339589463cf8d2c
Brian Balsamo <balsamo@uchicago.edu>
2016-07-08 14:24:42 -0500

    split APIResponse out into its own file.

--- hr_api.py
+++ hr_api.py
@@ -11,11 +11,14 @@ from hierarchicalrecord.hierarchicalrecord import HierarchicalRecord
 from hierarchicalrecord.recordconf import RecordConf
 from hierarchicalrecord.recordvalidator import RecordValidator
 
+from apiresponse import APIResponse
+
 
 # Globals
 _ALPHANUM_PATTERN = regex_compile("^[a-zA-Z0-9]+$")
 _NUMERIC_PATTERN = regex_compile("^[0-9]+$")
 
+# TODO
 # Options that should probably be a config or something
 _STORAGE_ROOT = '/Users/balsamo/test_hr_api_storage'
 
@@ -23,7 +26,8 @@ _STORAGE_ROOT = '/Users/balsamo/test_hr_api_storage'
 # Most of these are abstracted because they should be hooked
 # to some kind of database model in the future
 #
-# Note to future self: Probably make these base functions delegators to
+# TODO
+# Probably make these base functions delegators to
 # implementation specific functions
 
 
@@ -223,64 +227,6 @@ class RecordCategory(object):
 
     title = property(get_title, set_title)
     records = property(get_records, set_records, del_records)
-
-
-class APIResponse(object):
-
-    _status = None
-    _data = None
-    _errors = None
-
-    def __init__(self, status, data=None, errors=None):
-        self.status = status
-        self.data = data
-        self.errors = errors
-
-    def get_status(self):
-        return self._status
-
-    def set_status(self, status):
-        if status not in ['success', 'fail']:
-            raise ValueError("status MUST be 'success' or 'fail'")
-        self._status = status
-
-    def get_data(self):
-        return self._data
-
-    def set_data(self, data):
-        if not isinstance(data, dict) and data is not None:
-            raise ValueError("Data must be a dict")
-        self._data = data
-
-    def get_errors(self):
-        return self._errors
-
-    def set_errors(self, errors):
-        if errors is None:
-            self._errors = None
-            return
-        try:
-            self._errors = []
-            for x in errors:
-                self.add_error(x)
-        except TypeError:
-            raise ValueError("errors must be an iterable of strings")
-
-    def add_error(self, error):
-        if not isinstance(error, str):
-            raise ValueError("error must be a string")
-        self._errors.append(error)
-
-    def dictify(self):
-        r = {}
-        r['status'] = self.status
-        r['data'] = self.data
-        r['errors'] = self.errors
-        return r
-
-    errors = property(get_errors, set_errors)
-    data = property(get_data, set_data)
-    status = property(get_status, set_status)
 
 
 class RecordsRoot(Resource):
